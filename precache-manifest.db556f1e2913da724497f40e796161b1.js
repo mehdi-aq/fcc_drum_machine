dc5888692904c9de9233721b6a8dfe959a87d92b
mehdi_aq <douahmehdi.p@gmail.com>
2022-06-09 05:12:27 +0200

    Initial commit

---
+++ precache-manifest.db556f1e2913da724497f40e796161b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef9174e23212441d238ed805fc759c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9994b5270be99aefd84",
+    "url": "/static/css/main.f0e538b7.chunk.css"
+  },
+  {
+    "revision": "b77561b7909ab91da18c",
+    "url": "/static/js/2.6cdaf783.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/static/js/2.6cdaf783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9994b5270be99aefd84",
+    "url": "/static/js/main.0eb063ae.chunk.js"
+  },
+  {
+    "revision": "cae89e4e06bfc44f9444",
+    "url": "/static/js/runtime-main.90ca95cf.js"
+  },
+  {
+    "revision": "2803116d50a5be32e5435aba7d4bb941",
+    "url": "/static/media/alarmClock.2803116d.ttf"
+  }
+]);
